edb40780af11948cb13df7e42b45b1b07cb72896
ERIC KELLY <eakelly220@gmail.com>
2018-05-18 04:48:57 +0000

    hardcode in some teams

--- lib/basketball/cli.rb
+++ lib/basketball/cli.rb
@@ -1,5 +1,12 @@
 class Basketball::CLI
   def call
-    puts "Hello"
+    puts "Hello welcome to the basketball cli please enter the number of the team you would like to see"
+    puts"
+    1. Atlanta Hawks
+    2. Boston Celtics
+    3. Broklyn Nets
+    4. Charlote Hornets
+    5. Chicago Bulls
+    "
   end
 end
