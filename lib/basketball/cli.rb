adf3b117febb8e6bf822604a8e57562022a69e7d
ERIC KELLY <eakelly220@gmail.com>
2018-05-18 19:32:20 +0000

    Added a get player method that provides the stats of players.

--- lib/basketball/cli.rb
+++ lib/basketball/cli.rb
@@ -38,21 +38,51 @@ class Basketball::CLI
         when "1"
           puts "Give info about The Hawks, here are the players"
           list_players
+          get_player
         when "2"
           puts "Give info about The Celtics, here are the players"
           list_players
+          get_player
         when "3"
           puts "Give info about The Nets, here are the players"
           list_players
+          get_player
         when "4"
           puts "Give info about The Hornets, here are the players"
           list_players
+          get_player
         when "5"
           puts "Give info about The Bulls, here are the players"
           list_players
+          get_player
         else
           puts "Invalid Entry! Please enter a number of a team or exit"
           list_teams
+          get_player
+        end
+    end
+  end
+
+  def get_player
+    input = nil
+
+    while input != "exit"
+      input = gets.chomp
+      input = input.downcase
+      case input
+        when "1"
+          puts "Give the stats of Carmelo Anthony"
+        when "2"
+          puts "Give the stats of Lebron James"
+        when "3"
+          puts "Give the stats of Anthony Davis"
+        when "4"
+          puts "Give the stats of Kyrie Erving"
+        when "5"
+          puts "Give the stats of Kevin Durant"
+        else
+          puts "Invalid Entry! Please enter a number of a player or exit"
+          list_players
         end
     end
   end
