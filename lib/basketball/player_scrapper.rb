2cc5cc2245ae85a21f60dd87dbb449f7610ecdd9
ERIC KELLY <eakelly220@gmail.com>
2018-05-24 17:01:18 +0000

    added player number to scraper.

--- lib/basketball/player_scrapper.rb
+++ lib/basketball/player_scrapper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 
 class Basketball::PlayerScrapper
-  attr_accessor :name, :player_url
+
 
   def self.scrape_index_page(team_url)
     index = Nokogiri::HTML(open("http://nba.com#{team_url}"))
@@ -12,14 +12,19 @@ class Basketball::PlayerScrapper
 
     index.css(".nba-player-index__row").each do |player|
       player.css(".nba-player-index__trending-item").each do |items|
-      name = items.css(".nba-player-index__name").text
       link = items.css("a").attr("href").value
-      players << {name: name, player_url: link}
+      name = items.css("a").attr("title").value
+      number = items.css(".nba-player-trending-item__number").text
+      players << {name: name, player_url: link, number: number}
       end
     end
+    binding.pry
+
     players
   end
 
 end
 
 #<section class="nba-player-index__trending-item small-4 medium-3 large-2 team-atl-hawks">
+#<span class="nba-player-trending-item__number">24</span>
+#<div class="nba-player-index__details"><span>Guard</span><span><strong>6</strong> ft <strong>5</strong> in | <strong>201</strong> lbs</span></div>
